93cfb4b85919f5149e4a89f3d6fa11ee7dbe7372
Nitish N <nitish2k03@gmail.com>
2022-10-30 00:02:53 +0530

    feat: add login and profile page (#6)
    * feat(login, profile) : added login page and profile page
    
    * fix(profile): simplified code by creating common widget, made review changes, pull rebased
    
    * fix(profile) : added address fields
    
    * fix(profile) : added save button
    
    * fix(profile) : removed comments

--- lib/config/navigations.dart
+++ lib/config/navigations.dart
@@ -1,9 +1,13 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:thirumathikart_seller/bindings/home_bindings.dart';
+import 'package:thirumathikart_seller/bindings/login_bindings.dart';
 import 'package:thirumathikart_seller/bindings/add_edit_products_binding.dart';
+import 'package:thirumathikart_seller/bindings/profile_bindings.dart';
 import 'package:thirumathikart_seller/constants/navigation_routes.dart';
 import 'package:thirumathikart_seller/views/add_edit_product_page.dart';
 import 'package:thirumathikart_seller/views/home_page.dart';
+import 'package:thirumathikart_seller/views/login.dart';
+import 'package:thirumathikart_seller/views/profile.dart';
 
 class NavigationPages {
   static List<GetPage<dynamic>> getPages() => [
@@ -21,6 +25,16 @@ class NavigationPages {
           name: NavigationRoutes.addProduct,
           page: AddEditProductPage.new,
           binding: AddEditProductsBindings(),
+        ),
+        GetPage(
+          name: NavigationRoutes.loginRoute,
+          page: () => const LoginPage(),
+          binding: LoginBindings(),
+        ),
+        GetPage(
+          name: NavigationRoutes.profileRoute,
+          page: () => const ProfilePage(),
+          binding: ProfileBindings(),
         ),
       ];
 }
--- lib/constants/navigation_routes.dart
+++ lib/constants/navigation_routes.dart
@@ -2,4 +2,6 @@ class NavigationRoutes {
   static String home = '/home';
   static String addProduct = '/addProduct';
   static String editProduct = '/editProduct';
+  static String loginRoute = '/login';
+  static String profileRoute = '/profile';
 }
