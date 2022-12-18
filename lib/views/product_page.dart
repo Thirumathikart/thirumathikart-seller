638d1a5764deb30523a12061107a30fcf3236bca
Ram-20062003 <ramajs2003@gmail.com>
2022-12-19 04:21:54 +0530

    fix: edit-page with api

--- lib/views/product_page.dart
+++ lib/views/product_page.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:thirumathikart_seller/config/themes.dart';
+import 'package:thirumathikart_seller/constants/add_edit_product_constants.dart';
 import 'package:thirumathikart_seller/constants/api_constants.dart';
 import 'package:thirumathikart_seller/controllers/product_controller.dart';
 import 'package:thirumathikart_seller/widgets/app_bar.dart';
@@ -115,13 +116,19 @@ class ProductPage extends GetView<ProductController> {
                             onPressed: () => {
                               Get.toNamed(NavigationRoutes.editProduct,
                                   arguments: Product(
-                                      name: 'product_name',
-                                      price: '100',
-                                      imageUrl:
-                                          'https://aaveg.in/22/assets/images/team/110120080.jpg',
-                                      quantity: '100',
-                                      description: 'okay',
-                                      category: 'Two'))
+                                    name: state[index].product!.title,
+                                    price:
+                                        state[index].product!.price.toString(),
+                                    imageUrl:
+                                        '${ApiConstants.orderBaseUrl}/${state[index].imageUrl!}',
+                                    quantity:
+                                        state[index].product!.stock.toString(),
+                                    description:
+                                        state[index].product!.description,
+                                    category:
+                                        EditProductConstants.categoryItems[
+                                            state[index].product!.categoryId!],
+                                  ))
                             },
                           ),
                         ),
@@ -134,137 +141,5 @@ class ProductPage extends GetView<ProductController> {
             itemCount: state!.length,
           ),
         ),
-        // body: SingleChildScrollView(
-        //   physics: const NeverScrollableScrollPhysics(),
-        //   child: Column(
-        //     children: [
-        //       SingleChildScrollView(
-        //         child: controller.obx(
-        //           (state) => Expanded(
-        //             child: Container(
-        //               padding: const EdgeInsets.only(bottom: 20),
-        //               height: MediaQuery.of(context).size.height - 90,
-        //               child: ListView.builder(
-        //                 itemBuilder: (ctx, index) => Column(children: [
-        //                   Container(
-        //                     height: 190 >
-        //                             ((MediaQuery.of(context).size.height - 60) /
-        //                                 4.3)
-        //                         ? 190
-        //                         : ((MediaQuery.of(context).size.height - 60) /
-        //                             4.3),
-        //                     margin: const EdgeInsets.only(
-        //                         left: 10, right: 10, top: 8, bottom: 8),
-        //                     padding: const EdgeInsets.all(10.0),
-        //                     decoration: BoxDecoration(
-        //                       border: Border.all(color: AppTheme.textPrimary),
-        //                       borderRadius: BorderRadius.circular(10),
-        //                       color: AppTheme.textSecondary,
-        //                     ),
-        //                     child: Row(
-        //                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //                       crossAxisAlignment: CrossAxisAlignment.center,
-        //                       children: [
-        //                         Flexible(
-        //                             flex: 1,
-        //                             child: Container(
-        //                               decoration: BoxDecoration(
-        //                                 borderRadius:
-        //                                     BorderRadius.circular(5.0),
-        //                                 image: DecorationImage(
-        //                                   fit: BoxFit.cover,
-        //                                   image: AssetImage('https://thirumathikart.nitt.edu/api/product/${state![index].imageUrl}'),
-        //                                 ),
-        //                               ),
-        //                             )),
-        //                         const SizedBox(width: 10.0),
-        //                         Expanded(
-        //                           child: Column(
-        //                               crossAxisAlignment:
-        //                                   CrossAxisAlignment.start,
-        //                               mainAxisAlignment:
-        //                                   MainAxisAlignment.center,
-        //                               children: [
-        //                                 Padding(
-        //                                   padding:
-        //                                       const EdgeInsets.only(bottom: 10),
-        //                                   child: Text(
-        //                                     '${state[index].product!.title}',
-        //                                     style: const TextStyle(
-        //                                         fontWeight: FontWeight.bold,
-        //                                         fontSize: 16.0),
-        //                                   ),
-        //                                 ),
-        //                                 Padding(
-        //                                   padding:
-        //                                       const EdgeInsets.only(bottom: 10),
-        //                                   child: Text(
-        //                                     '₹ ${state[index].product!.price}',
-        //                                   ),
-        //                                 ),
-        //                                 Padding(
-        //                                   padding:
-        //                                       const EdgeInsets.only(bottom: 10),
-        //                                   child: Text(
-        //                                     '${state[index].product!.description}',
-        //                                   ),
-        //                                 ),
-        //                                 Padding(
-        //                                   padding:
-        //                                       const EdgeInsets.only(bottom: 10),
-        //                                   child: Text(
-        //                                     '${state[index].product!.title}',
-        //                                   ),
-        //                                 ),
-        //                                 Row(
-        //                                   children: [
-        //                                     const Text('Stocks Available: '),
-        //                                     Text(
-        //                                       '${state[index].product!.quantity}',
-        //                                     ),
-        //                                   ],
-        //                                 )
-        //                               ]),
-        //                         ),
-        //                         Column(
-        //                           mainAxisAlignment: MainAxisAlignment.start,
-        //                           crossAxisAlignment: CrossAxisAlignment.center,
-        //                           children: [
-        //                             SizedBox(
-        //                               width: 30.0,
-        //                               child: TextButton(
-        //                                 child: const Icon(
-        //                                   Icons.edit,
-        //                                   color: Colors.grey,
-        //                                 ),
-        //                                 onPressed: () => {
-        //                                   Get.toNamed(
-        //                                       NavigationRoutes.editProduct,
-        //                                       arguments: Product(
-        //                                           name: 'product_name',
-        //                                           price: '100',
-        //                                           imageUrl:
-        //                                               'https://aaveg.in/22/assets/images/team/110120080.jpg',
-        //                                           quantity: '100',
-        //                                           description: 'okay',
-        //                                           category: 'Two'))
-        //                                 },
-        //                               ),
-        //                             ),
-        //                           ],
-        //                         ),
-        //                       ],
-        //                     ),
-        //                   ),
-        //                 ]),
-        //                 itemCount: controller.length(),
-        //               ),
-        //             ),
-        //           ),
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
       );
 }
